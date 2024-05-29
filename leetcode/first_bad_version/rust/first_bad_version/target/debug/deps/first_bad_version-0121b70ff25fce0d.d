ce9173417cffe8c69375d5f4b2f5cf6ded3d11ef
JDN89 <jan.de.niels@gmail.com>
2024-05-29 22:27:54 +0200

    cleanup

--- leetcode/first_bad_version/rust/first_bad_version/target/debug/deps/first_bad_version-0121b70ff25fce0d.d
+++
@@ -1,5 +0,0 @@
-/home/jdn/Coding/Study_Algorithms/leetcode/first_bad_version/rust/first_bad_version/target/debug/deps/libfirst_bad_version-0121b70ff25fce0d.rmeta: src/main.rs
-
-/home/jdn/Coding/Study_Algorithms/leetcode/first_bad_version/rust/first_bad_version/target/debug/deps/first_bad_version-0121b70ff25fce0d.d: src/main.rs
-
-src/main.rs:
