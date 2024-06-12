2f2024cba231a1d9a044710346b3ea5ab490f518
JDN89 <jan.de.niels@gmail.com>
2024-06-12 19:35:38 +0200

    inverting a binary tree

--- leetcode/invert_binary_tree/main.c
+++ leetcode/invert_binary_tree/main.c
@@ -12,11 +12,51 @@
  */
 
 #include <stdio.h>
-struct TreeNode {
+#include <stdlib.h>
+typedef struct TreeNode {
   int val;
   struct TreeNode *left;
   struct TreeNode *right;
+} TreeNode;
+
+TreeNode *createNode(int value) {
+  TreeNode *result = malloc(sizeof(TreeNode));
+  if (result != NULL) {
+    result->left = NULL;
+    result->right = NULL;
+    result->val = value;
+  }
+  return result;
 };
+
+// NOTE: in the beginning I made the mistake of changing the values instead of
+// the comlete node adress This cause the children not to be swapped togheter
+// with their parents. Only the values where exchanged instead of the actual
+// connections
+TreeNode *invertTree(TreeNode *root) {
+  if (root == NULL) {
+    return root;
+  }
+
+  // NOTE: in case one of the arms of the AST is null -> don't create a temp
+  // TreeNode struct just swap the branches
+  if (root->left != NULL && root->right != NULL) {
+    struct TreeNode *temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+  } else if (root->left == NULL && root->right != NULL) {
+    root->left = root->right;
+    root->right = NULL;
+  } else if (root->left != NULL && root->right == NULL) {
+    root->right = root->left;
+    root->left = NULL;
+  }
+
+  invertTree(root->left);
+
+  invertTree(root->right);
+  return root;
+}
 
 void printNode(struct TreeNode *child) {
 
@@ -30,8 +70,23 @@ void printNode(struct TreeNode *child) {
   printNode(child->right);
 }
 
-struct TreeNode *invertTree(struct TreeNode *root) {
+int main() {
 
-  printNode(root);
-  return root;
+  TreeNode *n1 = createNode(4);
+  TreeNode *n2 = createNode(2);
+  TreeNode *n3 = createNode(1);
+  TreeNode *n4 = createNode(3);
+  TreeNode *n5 = createNode(7);
+  TreeNode *n6 = createNode(6);
+  TreeNode *n7 = createNode(9);
+
+  n1->left = n2;
+  n1->right = n5;
+  n2->left = n3;
+  n2->right = n4;
+  n5->left = n6;
+  n5->right = n7;
+
+  invertTree(n1);
+  printNode(n1);
 }
