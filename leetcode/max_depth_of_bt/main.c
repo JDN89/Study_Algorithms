ea2d47b4d25f80ff8b48b835ba02dd4f36df412c
JDN89 <jan.de.niels@gmail.com>
2024-06-23 22:40:02 +0200

    max depth -> debugging required

--- leetcode/max_depth_of_bt/main.c
+++ leetcode/max_depth_of_bt/main.c
@@ -32,6 +32,8 @@
 //     RETURN depth
 //
 // QUeue is FIFO
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct TreeNode {
@@ -40,23 +42,90 @@ typedef struct TreeNode {
   struct TreeNode *right;
 } TreeNode;
 
+typedef struct QueueNode {
+  struct TreeNode *node;
+  struct QueueNode *next;
+} QueueNode;
+
 typedef struct {
-  TreeNode *head;
-  TreeNode *tail;
+  QueueNode *head;
+  QueueNode *tail;
   int size;
 } Queue;
+
+bool isQueueEmpty(Queue *q) { return q->head == NULL; }
+
+void initQueue(Queue *q) {
+  q->head = NULL;
+  q->tail = NULL;
+}
 
 // tail not null -> attach to tail
 // head is null -> head is node
 void enqueue(Queue *q, TreeNode *node) {
-  TreeNode *newNode = malloc(sizeof(TreeNode));
+  QueueNode *newNode = malloc(sizeof(QueueNode));
+  newNode->node = node;
+  if (q->tail != NULL) {
+    q->tail->next = newNode;
+    q->tail = newNode;
+  }
+  if (q->head == NULL) {
+    q->tail = q->head = newNode;
+  }
+  q->size += 1;
 }
 
-// if head == NULL -> queue is empty and we should reutrn an error
-// temp store head
-// head = temp.next
-// if head ==null tail ==null
-// free temp
-// deQueue() {}
+TreeNode *dequeueu(Queue *q) {
+  if (isQueueEmpty(q)) {
+    return NULL;
+  }
+  QueueNode *temp = q->head;
+  q->head = q->head->next;
+  if (q->head == NULL) {
+    q->tail = NULL;
+  }
 
-int maxDepth(struct TreeNode *root) {}
+  TreeNode *node = temp->node;
+  free(temp);
+  q->size -= 1;
+  return node;
+}
+
+void print_queue(Queue *q) {
+  QueueNode *current = q->head;
+
+  while (current != NULL) {
+    printf("value of current node: %d \n", current->node->val);
+    current = current->next;
+  }
+  printf("End reached \n");
+}
+
+int maxDepth(struct TreeNode *root) {
+
+  if (root == NULL) {
+    return 0;
+  }
+
+  Queue q;
+  initQueue(&q);
+  enqueue(&q, root);
+  int depth = 0;
+
+  while (!isQueueEmpty(&q)) {
+    // set values in current level and increase depth
+    int levelSize = q.size;
+    depth++;
+    for (int i = 0; i < levelSize; i++) {
+
+      TreeNode *head = dequeueu(&q);
+      if (head->left != NULL) {
+        enqueue(&q, head->left);
+      }
+      if (head->right != NULL) {
+        enqueue(&q, head->right);
+      }
+    }
+  }
+  return depth;
+}
