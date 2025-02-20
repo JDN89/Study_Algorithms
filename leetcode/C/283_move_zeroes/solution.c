f20a9cae916cc3d0cc8eef895a2e5b8b1c2083ce
JDN89 <jan.de.niels@gmail.com>
2025-02-20 21:36:34 +0100

    wrong solution debug tomorrow

--- leetcode/C/283_move_zeroes/solution.c
+++ leetcode/C/283_move_zeroes/solution.c
@@ -1,23 +1,30 @@
 #include "solution.h"
 
-// reutrn earley if numsSize is 0 or 1 or nums == 0
-// loop over array and if you encounter a 0 , insert it at end of array
-// with each move doe numsSize -1  -number move
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+// loop over array en hou de laatste niet null positie bij
+// wanneer huidige waarde niet null is zeg je nums[insertdPost ++] = nums[i]
+// insertedPos accessed huidige waarde en doet dan pas + 1
+// op die manier doe je insertedpos enkel + 1 wanneer je een niet 0 waarde
+// tegenkomt en verhoog je het niet wanneer de waarde 0 is
+
+// move the non zeros forward and keep track of last moved position;
 void moveZeroes(int *nums, int numsSize) {
   if (numsSize == 0 || numsSize == 1 || nums == NULL) {
     return;
   }
+  int insertedpos = 0;
+  for (int i = 0; i <= numsSize; i++) {
+    if (nums[i] != 0) {
+      nums[insertedpos++] = nums[i];
+    }
+  }
+  if (insertedpos != numsSize) {
 
-  // when you encounter non zero -> move forward
-  // for over arr
-  // if (arr[i] != 0) { arr[insertedPos++] = arr[i]}
-  // er zal een verschil zijn tss insertedPos en nums[i] want inserted increased
-  // enkel voor niet 0 vul wat er overblijft op met 0 values 1,0,4,0,0 [1],...
-  // nums[insertedpos ++] -> access at 0 and THEN increase
-  // 1,[0] -> nothing
-  // 1,0,[4] 0> insertedpos == 1  \\ i == 2
-  //
+    for (int j = ++insertedpos; j <= numsSize; j++) {
+      nums[j] = 0;
+    }
+  }
 }
