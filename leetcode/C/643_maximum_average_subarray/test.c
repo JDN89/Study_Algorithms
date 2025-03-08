75b6c368fd56a3a93acf3fc44e058a37f63fe04d
JDN89 <jan.de.niels@gmail.com>
2025-03-08 19:29:18 +0100

    init find maximum average

--- leetcode/C/643_maximum_average_subarray/test.c
+++ leetcode/C/643_maximum_average_subarray/test.c
@@ -1,10 +1,9 @@
 #include "solution.h"
 #include <assert.h>
 
-int nums[6] = {1, 12, -5, -6, 50, 3};
-int k = 4;
-int numsSize = 6;
-
 void test_findMaximumAverage() {
+  int nums[6] = {1, 12, -5, -6, 50, 3};
+  int k = 4;
+  int numsSize = 6;
   assert(findMaxAverage(nums, numsSize, k) == 12.75);
 }
